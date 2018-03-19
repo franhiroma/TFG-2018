aa73a1efbdce17b5b1b64aaafedb501ba205d3b4
Fran <fran.hiroma@gmail.com>
2018-03-19 18:31:08 +0100

    Menú del juego (v0.01)
    Un menú muy básico que permite jugar (PLAY) o salir de la aplicación
    (EXIT).
    
    Hecho siguiendo el tutorial de
    https://www.youtube.com/watch?v=zc8ac_qUXQY

---
+++ TFG/Assets/Scripts/menu_principal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu_principal : MonoBehaviour {
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
