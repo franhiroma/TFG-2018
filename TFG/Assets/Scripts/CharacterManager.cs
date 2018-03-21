77cdec8d36ebdafa8066ed8ff57ce7c2cde07359
Fran <fran.hiroma@gmail.com>
2018-03-21 17:30:48 +0100

    Animación básica del cientifico  (v0.01)
    Animación básica en los dos ejes de dirección (X-+ e Y-+) del personaje
    principal del juego.

---
+++ TFG/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour {
+
+    Animator anim;
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetInteger("AnimState", 0);
+        anim.SetBool("isMoving", false);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetInteger("AnimState", 0);
+            anim.SetBool("isMoving", true);
+
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetInteger("AnimState", 1);
+            anim.SetBool("isMoving", true);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetInteger("AnimState", 2);
+            anim.SetBool("isMoving", true);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetInteger("AnimState", 3);
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+    }
+}
