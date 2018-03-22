07fe633c5226c65c8676085f02fc67f34da9b707
Fran <fran.hiroma@gmail.com>
2018-03-22 17:13:10 +0100

    Movimiento de personaje (v0.01)
    Movimiento básico del personaje.

--- TFG/Assets/Scripts/CharacterManager.cs
+++ TFG/Assets/Scripts/CharacterManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CharacterManager : MonoBehaviour {
 
     Animator anim;
+    private Rigidbody2D characterBody;
+    public float characterSpeed = 3.0F;
 
     // Use this for initialization
     void Start ()
@@ -12,36 +14,41 @@ public class CharacterManager : MonoBehaviour {
         anim = GetComponent<Animator>();
         anim.SetInteger("AnimState", 0);
         anim.SetBool("isMoving", false);
+        characterBody = GetComponent<Rigidbody2D>();
 
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-
         if (Input.GetKey(KeyCode.D))
         {
             anim.SetInteger("AnimState", 0);
             anim.SetBool("isMoving", true);
+            characterBody.velocity = Vector2.right * characterSpeed; // x = 1, y = 0
 
         }
         else if (Input.GetKey(KeyCode.W))
         {
             anim.SetInteger("AnimState", 1);
             anim.SetBool("isMoving", true);
+            characterBody.velocity = Vector2.up * characterSpeed; // x = 0, y = 1
         }
         else if (Input.GetKey(KeyCode.A))
         {
             anim.SetInteger("AnimState", 2);
             anim.SetBool("isMoving", true);
+            characterBody.velocity = Vector2.left * characterSpeed; // x = -1, y = 0
         }
         else if (Input.GetKey(KeyCode.S))
         {
             anim.SetInteger("AnimState", 3);
             anim.SetBool("isMoving", true);
+            characterBody.velocity = Vector2.down * characterSpeed; // x = 0, y = -1
         }
         else
         {
+            characterBody.velocity = Vector2.zero; // x = 0, y = 0 
             anim.SetBool("isMoving", false);
         }
     }
