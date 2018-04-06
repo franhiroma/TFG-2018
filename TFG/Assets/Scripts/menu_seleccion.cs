153c43d80bade6296a4e00a0d86b115bab380b21
Fran <fran.hiroma@gmail.com>
2018-04-06 20:07:39 +0200

    Menú de selección de niveles (v0.01)

---
+++ TFG/Assets/Scripts/menu_seleccion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu_seleccion : MonoBehaviour
+{
+
+    public void Seleccion()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
